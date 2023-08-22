db7d258b2c540173f965396b6472793a1f308e03
a00663044 <antoine.grosnit2@huawei.com>
2023-08-22 22:21:45 +0100

    [TEST]: add tests.

--- MCBO/tests/optimizers/non_bo/test_local_search.py
+++ MCBO/tests/optimizers/non_bo/test_local_search.py
@@ -1,22 +1,20 @@
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
 ROOT_PROJECT = str(Path(os.path.realpath(__file__)).parent.parent)
 sys.path[0] = ROOT_PROJECT
 
-import torch
-
-from mcbo.optimizers.non_bo.local_search import LocalSearch
-from mcbo.utils.plotting_utils import plot_convergence_curve
 
 if __name__ == '__main__':
+    from mcbo.optimizers.non_bo.hill_climbing import HillClimbing
+    from mcbo.utils.plotting_utils import plot_convergence_curve
     from mcbo.task_factory import task_factory
 
     task = task_factory('levy', num_dims=10, variable_type='nominal', num_categories=21)
     search_space = task.get_search_space()
 
-    optimizer = LocalSearch(search_space, input_constraints=task.input_constraints)
+    optimizer = HillClimbing(search_space, input_constraints=task.input_constraints)
 
     for i in range(500):
         x_next = optimizer.suggest(1)
