a22eedd6bb201131cf257d01a14e4394c0595b82
Antoine Grosnit <33458508+AntGro@users.noreply.github.com>
2024-06-06 09:43:27 +0100

    Fix utils_task_test.py

--- MCBO/mcbo/utils/utils_task_test.py
+++ MCBO/mcbo/utils/utils_task_test.py
@@ -5,10 +5,16 @@ from mcbo.tasks import TaskBase
 
 
 def test_task(task: TaskBase, batch_size: int = 4, n_evals: int = 10):
+    obj_dims = [0]
+    out_constr_dims = None
+    out_upper_constr_vals = None
     optimizer = RandomSearch(
         search_space=task.get_search_space(),
         input_constraints=task.input_constraints,
-        store_observations=False
+        obj_dims=obj_dims,
+        out_constr_dims=out_constr_dims,
+        out_upper_constr_vals=out_upper_constr_vals,
+        store_observations=False,
     )
 
     print(f"{optimizer.name}_{task.name}")
@@ -19,4 +25,7 @@ def test_task(task: TaskBase, batch_size: int = 4, n_evals: int = 10):
         y_next = task(x_next)
         print(f'Iteration {i + 1:>4d} - BBOX evaluation took {time.time() - ref_time:.2f}s')
         optimizer.observe(x_next, y_next)
-        print(f'Iteration {i + 1:>4d} - Best f(x) {optimizer.best_y:.3f}')
+        if len(obj_dims) == 1:
+            print(f'Iteration {i + 1:>4d} - Best f(x) {optimizer.best_y[obj_dims].item():.3f}')
+        else:
+            print(f'Iteration {i + 1:>4d} - Best f(x) {optimizer.best_y}')
