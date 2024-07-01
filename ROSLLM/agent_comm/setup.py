881db8bbb5b19f005fb18a037af97e493fc7b1e5
Christopher Mower 84310777 <christopher.mower@huawei.com>
2024-07-01 08:52:30 +0100

    Add agent_comm package

---
+++ ROSLLM/agent_comm/setup.py
@@ -0,0 +1,13 @@
+## ------------------------------------------------------------ ##
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD ! ##
+## ------------------------------------------------------------ ##
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=["agent_comm"],
+    package_dir={"": "src"},
+)
+
+setup(**setup_args)
