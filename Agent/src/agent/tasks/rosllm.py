f3b2c40d3b93e86355088630a93bd7fa59c14cd7
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2024-07-27 13:16:50 +0100

    demo

---
+++ Agent/src/agent/tasks/rosllm.py
@@ -0,0 +1,46 @@
+import traceback
+
+import rospy
+from rosllm_srvs.srv import Observation, ExecuteBehavior
+
+from agent.memory import MemKey
+from agent.tasks import Task
+
+
+class ROSLLM(Task):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        rospy.init_node("agent_node")
+        self.ros_resp = None
+
+    def reset(self):
+        return self.get_obs()
+
+    def get_obs(self):
+        rospy.wait_for_service("get_observation")
+        try:
+            get_obs = rospy.ServiceProxy("get_observation", Observation)
+            obs = get_obs().observation
+        except:
+            err = traceback.format_exc()
+            obs = f"[ERROR] failed to get observation from ROS, exception:\n{err}"
+        return {MemKey.OBSERVATION: obs}
+
+    def get_reward(self):
+        return float(self.ros_resp.success)
+
+    def get_done(self):
+        return False  # TODO
+
+    def execute_behavior(self, action):
+        rospy.wait_for_service("execute_behavior")
+        try:
+            execute_behavior = rospy.ServiceProxy("execute_behavior", ExecuteBehavior)
+            self.ros_resp = execute_behavior(action)
+        except:
+            pass
+
+    def step(self, action):
+        self.execute_behavior(action)
+        return self.get_obs(), self.get_reward(), self.get_done()
